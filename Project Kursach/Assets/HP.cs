09d7d3158702ef7356e09beb68cc61dd29f6533c
Oksana2097 <oksana.salomacheva@yandex.ru>
2018-05-18 22:46:40 +0700

    18.05.18

---
+++ Project Kursach/Assets/HP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour {
+    public int maxhp;
+    public int hp;
+    public Slider slide;
+
+	// Use this for initialization
+	void Start () {
+        maxhp = 100;
+        hp = maxhp;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        slide.value = hp;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            hp -= 25;
+        }
+        if (hp > maxhp)
+        {
+            hp = maxhp;
+
+        }
+        if (hp < 0)
+        {
+            hp = 0;
+        }
+	}
+}
---
+++ Project Kursach/Assets/dialog.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dialog : MonoBehaviour
+{
+
+    [TextArea(3,5)]
+    public string labelText;
+    public bool hasCollider;
+
+    void OnGUI()
+    {
+        if (hasCollider == true)
+        {
+            GUI.Box(new Rect(145, Screen.height - 100, Screen.width - 300, 120), labelText);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        hasCollider = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+       // hasCollider = false;
+        Destroy(gameObject);
+    }
+}
