a3c260257dad751e96f28f011d4abae9c09066a0
Oksana2097 <oksana.salomacheva@yandex.ru>
2018-05-20 20:04:55 +0700

    Door

---
+++ Project Kursach/Assets/Door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField]
+    float openDoor;
+    [SerializeField]
+    float closeDoor;
+    [SerializeField]
+    float speed = 1;
+
+    public bool isOpen;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Проверка открыта ли дверь.
+	void Update () {
+        if (isOpen)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+	}
+
+    void OpenDoor() 
+    {                                                                                               // угол поворота openDoor          // скорость поворота 
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.x, openDoor, transform.rotation.z), speed * Time.deltaTime);	
+     }
+     
+    void CloseDoor()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.rotation.x, closeDoor, transform.rotation.z), speed * Time.deltaTime);
+
+
+    }
+
+}
---
+++ Project Kursach/Assets/DoorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+    public float distance = 2f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width/2, Screen.height / 2));
+             RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                if (hit.collider.tag == "Door")
+                {
+                    Door door = hit.collider.GetComponent<Door>();
+                    door.isOpen = !door.isOpen; //Противоположное значение
+                }
+            }
+        }
+		
+	}
+}
