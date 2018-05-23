a42fb68d5e3a3378a025779791df933c40b4c7dc
Oksana2097 <oksana.salomacheva@yandex.ru>
2018-05-23 20:35:57 +0700

    добавила монстров

---
+++ Project Kursach/Assets/True_Horror/e_scrarecrow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class e_scrarecrow : MonoBehaviour
+{
+
+    public GameObject player;
+    public float dist;
+    NavMeshAgent Nav;
+    public float Radius;
+
+    // Use this for initialization
+    void Start()
+    {
+        Nav = GetComponent<NavMeshAgent>();
+
+    }
+    void Update()
+    {
+
+        dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist > Radius)
+        {
+            Nav.enabled = false;
+            gameObject.GetComponent<Animator>().SetTrigger("Idle");
+        }
+        if (dist < Radius)
+        {
+            Nav.enabled = true;
+            Nav.SetDestination(player.transform.position);
+            gameObject.GetComponent<Animator>().SetTrigger("walk");
+            Radius = 40;
+
+        }
+
+    }
+}
